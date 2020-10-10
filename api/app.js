6959d1c479e50a90101848dd30dc720b1d97287c
lavsexpert <lavrov-sergey@yandex.ru>
2020-10-10 09:50:58 +0300

    Получение пользователя по id

--- api/app.js
+++ api/app.js
@@ -13,6 +13,24 @@ app.get("/api/v1/users", (req, res) => {
   res.send(users);
 })
 
+app.get("/api/v1/users/:id", (req, res) => {
+  var id = req.params.id;
+  var content = fs.readFileSync("users.json", "utf8");
+  var users = JSON.parse(content);
+  var user = null;
+  for (var i = 0; i < users.lenght; i++) {
+    if(users[i].id == id){
+      user = users[i];
+      break;
+    }
+  }
+  if (user){
+    res.send(user);
+  } else {
+    res.status(404).send("Пользователь не найден");
+  }
+})
+
 // Слушаем порт 3000
 app.listen(3000, () => {
   console.log("Сервер ожидает подключения...");
