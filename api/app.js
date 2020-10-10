12bb8e7b1f2d201fbf01be189ccdeb194a09c12e
lavsexpert <lavrov-sergey@yandex.ru>
2020-10-10 09:44:40 +0300

    Создали GET users

---
+++ api/app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var fs = require("fs");
+
+var app = express();
+var jsonParser = bodyParser.json();
+app.use(express.static(__dirname + "/public"));
+
+// Получение списка пользователей
+app.get("/api/v1/users", (req, res) => {
+  var content = fs.readFileSync("users.json", "utf8");
+  var users = JSON.parse(content);
+  res.send(users);
+})
+
+// Слушаем порт 3000
+app.listen(3000, () => {
+  console.log("Сервер ожидает подключения...");
+})
