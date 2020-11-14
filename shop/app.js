e5cb28d2a8abb2705c88838ad6e45fa1b4771cd5
lavsexpert <lavrov-sergey@yandex.ru>
2020-11-14 11:18:32 +0300

    Создали основу для магазина

---
+++ shop/app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var fs = require("fs");
+var sqlite3 = require("sqlite3").verbose();
+
+var app = express();
+var jsonParser = bodyParser.json();
+app.use(express.static(__dirname + "/public"));
+//var db = new sqlite3.Database(':memory:');
+var db = new sqlite3.Database('db.db');
+
+// Слушаем порт 3000
+app.listen(3000, () => {
+  console.log("Сервер ожидает подключения...");
+});
+
+process.on('SIGINT', () => {
+    db.close();
+    server.close();
+});
